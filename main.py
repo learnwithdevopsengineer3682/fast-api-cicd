c8b21e25f73f08b870378087ca9217177709f33d
learnwithdevopsengineer3682 <learnwithdevopsengineer3682@gmail.com>
2025-06-27 18:38:05 +0200

    Add files to the repo

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/")
+def hello():
+    return {"msg": "It works! Auto-deployed by GitHub Actions"}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+class ReverseRequest(BaseModel):
+    text: str
+
+@app.post("/reverse")
+def reverse(req: ReverseRequest):
+    reversed_text = req.text[::-1]
+    return {"reversed": reversed_text}
